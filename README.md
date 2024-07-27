6aa34fe2da6e69ecc2f1f19c8e3a0bb3d8f3aaeb
JonathanDudzik <jonathan.d.wesley@gmail.com>
2024-07-27 07:22:24 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Resources
+<https://inertiajs.com/>
+<https://vuejs.org/>
+<https://laravel.com/docs/11.x>
+
+# New Theme
+## Oh So Many Books
+    beds → pages
+    Number of pages in the book.
+
+    baths → chapters
+    Number of chapters in the book.
+
+    area → width
+    Width of the book cover in millimeters.
+
+    city → publisher
+    Publisher name or location.
+
+    code → isbn
+    International Standard Book Number (ISBN) for the book.
+
+    street → author
+    Author’s last name.
+
+    street_nr → title
+    Title of the book.
+
+    price → price
+    Price of the book.
---
+++ database/migrations/2022_09_23_180153_create_listings_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('listings', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->unsignedTinyInteger('beds');
+            $table->unsignedTinyInteger('baths');
+            $table->unsignedSmallInteger('area');
+
+            $table->tinyText('city');
+            $table->tinyText('code');
+            $table->tinyText('street');
+            $table->tinyText('street_nr');
+
+            $table->unsignedInteger('price');
+            $table->foreignIdFor(
+                \App\Models\User::class,
+                'by_user_id'
+            )->constrained('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('listings');
+    }
+};
