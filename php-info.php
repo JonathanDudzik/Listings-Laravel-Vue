a19febe22667d718625658010c2de8e7d7756a08
JonathanDudzik <jonathan.d.wesley@gmail.com>
2024-07-28 08:45:24 -0400

    trouble shooting

---
+++ php-info.php
@@ -0,0 +1,9 @@
+<?php
+/*
+  * If you are concerned about what PHP version your Apache server is using, 
+  * the output of phpinfo() is always what you should pay attention to. 
+  * The Apache PHP module and the PHP command line binary are two different things 
+  * that don’t interfere with each other.
+*/
+phpinfo();
+?>
